5c82bc963b9a49008e32382fa8e8184789afaed8
hwk0911@gmail.com <hwk0911@gmail.com>
2020-03-06 18:55:11 +0900

    HelloResponseDto.java
    Hello Controller를 롬복으로 전환하기
    
    세부 항목에 대한 주석 추가 완료

---
+++ src/main/java/com/tistory/cafecoder/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,20 @@
+package com.tistory.cafecoder.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter // 1
+@RequiredArgsConstructor // 2
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
+
+/*
+1 : @Getter
+    선언된 모든 필드의 get 메소드를 생성해 준다.
+
+2 : @RequiredArgsConstructor
+    선언된 모든 final 필드가 포함된 생성자를 생성해 준다.
+    final이 없는 필드는 생성자에 포함되지 않는다.
+ */
