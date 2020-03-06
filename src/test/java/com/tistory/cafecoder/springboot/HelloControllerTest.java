b9ff999214dc3d326d80eff5534bef8ed1ad3201
hwk0911@gmail.com <hwk0911@gmail.com>
2020-03-06 19:47:16 +0900

    public void helloDto가_리턴된다() 함수 추가
    param, JSON 주석 추가 완료.

--- src/test/java/com/tistory/cafecoder/springboot/HelloControllerTest.java
+++ src/test/java/com/tistory/cafecoder/springboot/HelloControllerTest.java
@@ -11,9 +11,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) // 1
 @WebMvcTest(controllers = HelloController.class) // 2
@@ -29,6 +29,22 @@ public class HelloControllerTest {
         mvc.perform(get("/hello")) // 5
                 .andExpect(status().isOk()) // 6
                 .andExpect(content().string(hello)); // 7
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+
+
+
     }
 }
 
@@ -66,4 +82,15 @@ public class HelloControllerTest {
     mvc.perform의 결과를 검증한다.
     응답 본문의 내용을 검증한다.
     Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증한다.
+
+8 : param
+    API 테스트할 때 사용될 요청 파라미터를 설정한다.
+    단 값은 String만 허용된다. .param("amount", String.valueOf(amount))) 에서 int 변수 amount를
+    String.valueOf를 통해 사용했다.
+    그래서 숫자/날짜 등의 데이터도 등록할 때는 문자열로 변경해야만 가능하다.
+
+9 : josnPath
+    JSON 응답값을 필드별로 검증할 수 있는 메소드다.
+    $를 기준으로 필드명을 명시한다.
+    여기서는 name과 amount를 검증하니 $.name, $.amount로 검증한다.
  */
