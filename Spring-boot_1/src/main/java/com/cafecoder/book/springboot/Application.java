1af258ecdb12ee4c1bfd491ab8d9207277face31
hwk0911 <hwk0911@gmail.com>
2020-05-07 16:13:59 +0900

    Chpater 3 완료
    1. BaseTime을 사용한 작성시간, 수정시간 체크
    2. H2-Console을 사용한 현재 데이터베이스 확인

--- Spring-boot_1/src/main/java/com/cafecoder/book/springboot/Application.java
+++ Spring-boot_1/src/main/java/com/cafecoder/book/springboot/Application.java
@@ -2,10 +2,13 @@ package com.cafecoder.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
     }
 }
