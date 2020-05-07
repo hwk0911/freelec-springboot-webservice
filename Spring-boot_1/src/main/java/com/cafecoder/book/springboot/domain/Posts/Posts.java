807b1f89b3dcbf5d6787b4c71b45d93beb9facbd
hwk0911 <hwk0911@gmail.com>
2020-05-07 15:04:08 +0900

    Chpater 3 중간 저장
    1. Jpa를 사용한 타이틀, 본문을 갖는 데이터 저장
    2. Hibernate 기능을 통해 Spring 로딩 중 sql문 확인
    3. Hibernate 문법을 MySQL과 유사하게 나타나도록 변경

---
+++ Spring-boot_1/src/main/java/com/cafecoder/book/springboot/domain/Posts/Posts.java
@@ -0,0 +1,32 @@
+package com.cafecoder.book.springboot.domain.Posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ Spring-boot_1/src/test/java/com/cafecoder/book/springboot/domain/Posts/PostsRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.cafecoder.book.springboot.domain.Posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanUp() {
+        ///추후 테스트에서 이전 테스트에 영향이 없도록 모든 내용을 지운다.
+
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void readPosts() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("hwk0911@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
