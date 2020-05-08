e1e1cd9dec7e85320f62fc9c23c563366824475f
hwk0911 <hwk0911@gmail.com>
2020-05-08 19:04:29 +0900

    Chapter 5 중간 저장
    GCP와 SpringBoot2 - oauth2 를 사용한 로그인 구현

---
+++ Spring-boot_1/src/main/java/com/cafecoder/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -0,0 +1,49 @@
+package com.cafecoder.book.springboot.config.auth.dto;
+
+import com.cafecoder.book.springboot.domain.user.Role;
+import com.cafecoder.book.springboot.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        return ofGoogle(userNameAttributesName, attributes);
+    }
+
+    private static OAuthAttriutes ofGoogle(String userNameAttributes, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("pictrue"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.GUEST)
+                .build();
+    }
+}
