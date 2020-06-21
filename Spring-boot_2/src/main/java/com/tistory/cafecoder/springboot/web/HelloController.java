b10eba503c6316e8f8c6e23b1843e14a27b141eb
hwk0911 <hwk0911@gmail.com>
2020-06-21 18:09:07 +0900

    ResponseDto를 사용하도록 수정
    @RequestParam을 통해 API로 넘긴 값을 받아온다.

--- Spring-boot_2/src/main/java/com/tistory/cafecoder/springboot/web/HelloController.java
+++ Spring-boot_2/src/main/java/com/tistory/cafecoder/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.tistory.cafecoder.springboot.web;
 
+import com.tistory.cafecoder.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +10,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto (@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
 
@@ -19,4 +26,10 @@ public class HelloController {
 
 @GetMapping
 HTTP Method인 Get의 요청을 받을 수 있는 API를 만든다.
+
+@RequestParam
+외부에서 API로 넘긴 파라미터를 가져오는 어노테이션이다.
+여기서는 외부에서 name(@RequestParam("name"))이란 이름으로 넘긴 파라미터를
+메소드 파라미터 name(String name)에 저장하게 된다.
+
  */
