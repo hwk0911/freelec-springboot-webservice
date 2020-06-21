1ae79efaedd439015880592df15cb13e9226dfb6
hwk0911 <hwk0911@gmail.com>
2020-06-21 18:03:38 +0900

    기존 HelloController의 dto 생성
    lombok라이브러리 사용하여 선언

---
+++ Spring-boot_2/src/main/java/com/tistory/cafecoder/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,21 @@
+package com.tistory.cafecoder.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
+
+/*
+@Getter
+선언된 모든 필드의 get 메소드를 생성해준다.
+
+@RequiredArgsConstructor
+선언된 모든 final 필드가 포함된 생성자를 생성해준다.
+final이 없는 필드는 생성자에 포함되지 않는다.
+ */
