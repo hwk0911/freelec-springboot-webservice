973e4d2b6d6981be3ec4d669b5cb26ded8db496e
hwk0911 <hwk0911@gmail.com>
2020-06-23 18:12:58 +0900

    Post 등록에 대한 테스트코드 추가

---
+++ Spring-boot_2/src/test/java/com/tistory/cafecoder/springboot/web/PostsApiControllerTest.java
@@ -0,0 +1,75 @@
+package com.tistory.cafecoder.springboot.web;
+
+import com.tistory.cafecoder.springboot.domain.posts.Posts;
+import com.tistory.cafecoder.springboot.domain.posts.PostsRepository;
+import com.tistory.cafecoder.springboot.web.dto.PostsSaveRequestDto;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PostsApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @After
+    public void tearDown() throws Exception {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Posts_등록된다() throws Exception {
+        //given
+        String title = "title";
+        String content = "content";
+        String author = "hwk0911@gamil.com";
+
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+        assertThat(all.get(0).getAuthor()).isEqualTo(author);
+    }
+}
+
+/*
+Api Controller를 테스트하는데 HelloController와 달리 @WebMvcTest를 사용하지 않은 이유
+
+@WebMvcTest의 경우 JPA기능이 작동하지 않기 때문이다.
+Controller 와 ControllerAdvice 등 외부 연동과 관련된 부분만 활성화 되니 지금과 같이 JPA기능까지 한번에 테스트할 때는
+SpringBootTest와 TestRestTemplate을 사용하면 된다.
+ */
