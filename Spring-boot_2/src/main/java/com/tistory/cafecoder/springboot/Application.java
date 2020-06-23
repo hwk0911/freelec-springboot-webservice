3d560b2a89b48c877145627f80a2ea9fe198f50a
hwk0911 <hwk0911@gmail.com>
2020-06-23 18:56:01 +0900

    Jpa Auditing기능 활성화

--- Spring-boot_2/src/main/java/com/tistory/cafecoder/springboot/Application.java
+++ Spring-boot_2/src/main/java/com/tistory/cafecoder/springboot/Application.java
@@ -2,7 +2,9 @@ package com.tistory.cafecoder.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
@@ -14,4 +16,7 @@ public class Application {
 /*
 SpringApplication.run(Application.class, args);
 를 선언하면, 항상 서버에 톰캣을 설치할 필요가 없게되며, Jar파일로 실행하면 된다.
+
+@EnableJpaAuditing
+JPA Auditing 활성화
  */
