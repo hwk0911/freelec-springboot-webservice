ca5780142c79a19dad057a40a7716c4df34de6d0
hwk0911 <hwk0911@gmail.com>
2020-06-25 19:07:12 +0900

    업데이트로 이동하기 위한 컨트롤러 추가

--- Spring-boot_2/src/main/java/com/tistory/cafecoder/springboot/web/IndexController.java
+++ Spring-boot_2/src/main/java/com/tistory/cafecoder/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.tistory.cafecoder.springboot.web;
 
 import com.tistory.cafecoder.springboot.service.Posts.PostsService;
+import com.tistory.cafecoder.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -22,6 +24,14 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 
 }
