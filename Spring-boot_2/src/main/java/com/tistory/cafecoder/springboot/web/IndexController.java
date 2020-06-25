81f2880328f40947366f610b0148de6a29d2943b
hwk0911 <hwk0911@gmail.com>
2020-06-25 18:30:12 +0900

    model 추가

--- Spring-boot_2/src/main/java/com/tistory/cafecoder/springboot/web/IndexController.java
+++ Spring-boot_2/src/main/java/com/tistory/cafecoder/springboot/web/IndexController.java
@@ -1,13 +1,21 @@
 package com.tistory.cafecoder.springboot.web;
 
+import com.tistory.cafecoder.springboot.service.Posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
         return "index";
     }
 
@@ -15,6 +23,7 @@ public class IndexController {
     public String postsSave() {
         return "posts-save";
     }
+
 }
 
 /*
@@ -24,4 +33,8 @@ public class IndexController {
 "index"를 반환하므로, src/main/resources/templates/index.mustache로 전환되어 View Resolver가 처리하게 된다.
 
 ViewResolver는 URL요청의 결과를 전달할 타입과 값을 지정하는 관리자 격으로 볼 수 있다.
+
+Model
+서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
+여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달한다.
  */
