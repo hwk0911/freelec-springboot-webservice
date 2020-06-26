31f919b5c06b3873f1ee083ee810aee3129e1bda
hwk0911 <hwk0911@gmail.com>
2020-06-26 19:26:50 +0900

    삭제 작업 Transaction 추가

--- Spring-boot_2/src/main/java/com/tistory/cafecoder/springboot/service/Posts/PostsService.java
+++ Spring-boot_2/src/main/java/com/tistory/cafecoder/springboot/service/Posts/PostsService.java
@@ -44,6 +44,13 @@ public class PostsService {
                 .map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
     }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        postsRepository.delete(posts);
+    }
 }
 
 /*
@@ -79,4 +86,9 @@ postsRepository 결과로 넘어온 Posts의 Stream을 map을 통해 PostsListRe
 @Transactional(readOnly = true)
 readOnly를 인자로 추가하면, 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선된다.
 때문에, 등록, 수정, 삭제 기능이 전혀 없는 서비스 메소드에 사용하는 것을 추천한다.
+
+postsRepository.delete(posts)
+JpaRepository에서 이미 delete 메소드를 지원하고 있어 이것을 활용한다.
+엔티티를 파라미터로 삭제할 수 있고, deleteById 메소드를 사용하면 id로 삭제할 수 있다.
+존재하는 Posts인지 확인을 위해 엔티티 조회 후 그대로 삭제한다.
  */
