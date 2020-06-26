6cd6a7d40df7f322c5713057d5fcf9c77051805d
hwk0911 <hwk0911@gmail.com>
2020-06-26 15:52:40 +0900

    @Query 를 통해 데이터베이스 검색

--- Spring-boot_2/src/main/java/com/tistory/cafecoder/springboot/domain/posts/PostsRepository.java
+++ Spring-boot_2/src/main/java/com/tistory/cafecoder/springboot/domain/posts/PostsRepository.java
@@ -1,9 +1,13 @@
 package com.tistory.cafecoder.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
-
+    @Query("SELECT P FROM POSTS P ORDER BY P.ID DESC")
+    List<Posts> findAllDesc();
 }
 /*
 iBatis or Mybatis등에서 Dao라 불리는 DB Layer 접근자다.
