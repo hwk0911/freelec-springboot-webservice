5ae3319be957e3cc6888594c2ff6d87825966cd7
hwk0911 <hwk0911@gmail.com>
2020-06-29 17:47:35 +0900

    스캔 대상 제거

--- Spring-boot_2/src/test/java/com/tistory/cafecoder/springboot/web/HelloControllerTest.java
+++ Spring-boot_2/src/test/java/com/tistory/cafecoder/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.tistory.cafecoder.springboot.web;
 
+import com.tistory.cafecoder.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,13 +16,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.CoreMatchers.is;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void hello() throws Exception{
         String hello = "hello";
 
@@ -28,6 +35,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
