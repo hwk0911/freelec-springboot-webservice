4352ef2ae2b710f16aaef62990518eb52a66621b
hwk0911 <hwk0911@gmail.com>
2020-06-29 17:46:59 +0900

    인터페이스 누락 해결

--- Spring-boot_2/src/main/java/com/tistory/cafecoder/springboot/config/auth/dto/SessionUser.java
+++ Spring-boot_2/src/main/java/com/tistory/cafecoder/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.tistory.cafecoder.springboot.config.auth.dto;
 import com.tistory.cafecoder.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
