fcb8ded9ee3bd1165a19e1f8091e7c11b44496b3
hwk0911 <hwk0911@gmail.com>
2020-06-29 16:39:35 +0900

    세션 기능 개선을 위한 클래스

---
+++ Spring-boot_2/src/main/java/com/tistory/cafecoder/springboot/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,40 @@
+package com.tistory.cafecoder.springboot.config.auth;
+
+import com.tistory.cafecoder.springboot.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}
+
+/*
+supportsParameter
+컨트롤러 메소드의 특정 파라미터를 지원하는지 판단한다.
+여기서는 파라미터에 @LoginUser 어노테이션이 붙어 있고, 파라미터 클래스 타입이 SessionUser.class인 경우 true를 반환한다.
+
+resolveArgument
+파라미터에 전달할 객체를 생성한다.
+여기서는 세션에서 객체를 가져온다.
+ */
